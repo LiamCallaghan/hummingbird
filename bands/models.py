6682e26b3563b9031fce774346c2e4bc2fe06729
Liam Callaghan <ljcallaghan64@gmail.com>
2020-10-21 15:38:52 +0100

    users and band backend

---
+++ bands/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+goal_choices = (
+    ('1', 'Make music'),
+    ('2', 'Make money'),
+    ('3', 'Make friends')
+)
+
+# Create your models here.
+class Band(models.Model):
+    name = models.CharField(max_length=50)
+    # genre = models.CharField(max_length=50, many=True)
+    description = models.TextField()
+    goals = models.CharField(max_length=12, choices=goal_choices, default= '1')
+    size = models.PositiveIntegerField(validators=[MinValueValidator(2)], default= 2)
+    # city = models.CharField(max_length=255)
+    # location = PlainLocationField(based_fields=['city'], zoom=7)
---
+++ jwt_auth/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# instrument_choices = (
+    # ('VOC', 'Vocals'),
+    # ('DMK', 'Drum Kit'),
+    # ('LEG', 'Lead Guitar'),
+    # ('RYG', 'Rhythm Guitar'),
+    # ('BSG', 'Bass Guitar'),
+    # ('ACG', 'Acoustic Guitar'),
+    # ('UKE', 'Ukulele'),
+    # ('HRP', 'Harp'),
+    # ('BNJ', 'Banjo'),
+    # ('ACC', 'Accordion'),
+    # ('PAN', 'Piano'),
+    # ('KYB', 'Keyboard'),
+    # ('SAX', 'Saxophone'),
+    # ('TRP', 'Trumpet'),
+    # ('CLR', 'Clarinet'),
+    # ('FLT', 'Flute'),
+    # ('BGP', 'Bagpipe'),
+    # ('TRM', 'Trombone'),
+    # ('HRM', 'Harmonica'),
+    # ('OBE', 'Oboe'),
+    # ('CEL', 'Cello'),
+    # ('VLN', 'Violin'),
+    # ('DBB', 'Double Bass'),
+    # ('SNT', 'Synth'),
+    # ('DRM', 'Drum Machine'),
+# )
+
+skill_choices = (
+    ('1', 'Beginner'),
+    ('2', 'Intermediate'),
+    ('3', 'Expert')
+)
+
+# Create your models here.
+class User(AbstractUser):
+    email = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    # instrument = models.CharField(max_length=20, choices=instrument_choices)
+    skill_level = models.CharField(max_length=12, choices=skill_choices, default='1')
+    profile_image = models.CharField(max_length=300)
+    # city = models.CharField(max_length=255)
+    # location = PlainLocationField(based_fields=['city'], zoom=7)
