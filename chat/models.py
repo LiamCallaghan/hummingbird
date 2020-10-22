e7ec0a62ea53a19b5f6ae4c004b062e0953c8dc5
Liam Callaghan <ljcallaghan64@gmail.com>
2020-10-22 14:56:34 +0100

    changed model style for messages

--- chat/models.py
+++ chat/models.py
@@ -3,15 +3,17 @@ from django.db import models
 # Create your models here.
 class Chat(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    related_band = models.ManyToManyField(
+    related_band = models.ForeignKey(
         'bands.Band',
-        related_name='band'
+        related_name='chats',
+        on_delete=models.CASCADE
     )
-    recipient = models.ManyToManyField(
+    sender = models.ForeignKey(
         'jwt_auth.User',
-        related_name='recipient'
+        related_name='sent_chats',
+        on_delete=models.CASCADE
     )
     status = models.CharField(max_length=20, default='empty')
 
     def __str__(self):
-        return f'{self.recipient}'
+        return f'{self.sender} => {self.related_band}'
--- message/views.py
+++ message/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound, PermissionDenied
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers.common import MessageSerializer
+from .models import Message
+
+class MessageListView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, _request):
+        message_list = Message.objects.all()
+        serialized_message_list = MessageSerializer(message_list, many=True)
+        return Response(serialized_message_list.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        request.data['sent_by'] = request.user.id
+        message_to_create = MessageSerializer(data=request.data)
+        if message_to_create.is_valid():
+            message_to_create.save()
+            return Response(message_to_create.data, status=status.HTTP_201_CREATED)
+        return Response(message_to_create.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+class MessageDetailView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
+    def get_message(self, pk):
+        try:
+            return Message.objects.get(pk=pk)
+        except Message.DoesNotExist:
+            raise NotFound()
+
+    def get(self, _request, pk):
+        message = self.get_message(pk=pk)
+        serialized_message = MessageSerializer(message)
+        return Response(serialized_message.data, status=status.HTTP_200_OK)
