63e747bb2d2d0a9c3c86857116b376a921e5658c
Liam Callaghan <ljcallaghan64@gmail.com>
2020-10-23 11:59:25 +0100

    user components

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,14 +2,22 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3000/api'
 
-// const withHeaders = () => {
-//   return {
-//     headers: {
-//       Authorization: `Bearer ${localStorage.getItem('token')}`
-//     }
-//   }
-// }
+const withHeaders = () => {
+  return {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    }
+  }
+}
 
 export const getBandList = () => {
   return axios.get(`${baseUrl}/bands`)
+}
+
+export const getSingleProfile = profileId => {
+  return axios.get(`${baseUrl}/users/${profileId}`)
+}
+
+export const getUserProfile = () => {
+  return axios.get(`${baseUrl}/profile`, withHeaders())
 }
